5b47f98b93ab2f72ada9aff363a6921a4cc7bf0d
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-06-11 21:10:26 +0530

    Added Material UI and icons

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhNNK_xMG15LutJ5ZwPdjFdEa6uAuEZV4",
+    authDomain: "clone-f1033.firebaseapp.com",
+    projectId: "clone-f1033",
+    storageBucket: "clone-f1033.appspot.com",
+    messagingSenderId: "930243112548",
+    appId: "1:930243112548:web:84c99b62335dfed2f73971",
+    measurementId: "G-GKZZ21NMC9"
+  };
