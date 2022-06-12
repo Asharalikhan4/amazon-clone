6c579f3eba1fcddecab5571aa6c9c00d609eba17
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-06-12 14:26:07 +0530

    Added content in components

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -9,9 +9,9 @@ function Home() {
                 <img className='home_image' src='https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg' alt='Banner' />
                 <div className='home_row'>
                     <Product
-                     title='The lean startup'
-                     price={29.99}
-                     image='https://m.media-amazon.com/images/I/41Ag4WE7uyL._AC_UY436_FMwebp_QL65_.jpg' alt='book'
+                     title='Shelf Liner for Kitchen Cabinets, Non-Adhesive Drawer Liner,Refrigerator Liners, Reusable Cabinet Liner Drawer Mat Moisture-Proof Dust Anti-Slip Kitchen Table Pad (17.7 x47.2in, Bubble Ball)'
+                     price={807.25}
+                     image='https://m.media-amazon.com/images/I/61Oahg-xDqL._AC_SX679_.jpg' alt='book'
                      rating={3}
                      />
                     <Product 
@@ -22,12 +22,33 @@ function Home() {
                     />
                 </div>
                 <div className='home_row'>
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product 
+                        title='Sony WH-1000XM4 Wireless Premium Noise Canceling Overhead Headphones with Mic for Phone-Call and Alexa Voice Control, Black'
+                        price={22464.12}
+                        image='https://m.media-amazon.com/images/I/71o8Q5XJS5L._AC_SX679_.jpg'
+                        rating={5}
+                    />
+                    <Product 
+                        title='HIFIMAN Arya Stealth Magnet Version Full-Size Over-Ear Planar Magnetic Headphone for Audiophiles/Studio'
+                        price={104967.21}
+                        image='https://m.media-amazon.com/images/I/71lKmUUztfL._AC_SX679_.jpg'
+                        rating={3}
+                    />
+                    <Product 
+                        title='Sennheiser HD 560 S Over-The-Ear Audiophile Headphones - Neutral Frequency Response, E.A.R. Technology for Wide Sound Field, Open-Back Earcups, Detachable Cable, (Black) (HD 560S)'
+                        price={13932.60}
+                        image='https://m.media-amazon.com/images/I/8182xOzfS2L._AC_SX679_.jpg'
+                        rating={5}
+                    />
                 </div>
                 <div className='home_row'>
-                    <Product />
+                    <Product 
+                        title='WalkingPad Under Desk Treadmill, Smart Folding Treadmill with Adaptive Speed Technology, Maintains Preferred Pace, Foldable Treadmill for Home and Office, Stores Away Easily, A1Pro Black'
+                        price={44442.59}
+                        image='https://m.media-amazon.com/images/I/61G+P0QOVpL._AC_SX679_.jpg'
+                        rating={3}
+
+                    />
                 </div>
             </div>
         </div>
